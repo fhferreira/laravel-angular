dc2a695a0a34e38d0f6c8fa0692649f73f5a2858
Bob Kingstone <robert.kingstone@gmail.com>
2013-09-08 20:47:43 +0100

    Edited file names to tie in with blog post and edited readme

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('Blog', []).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'postsListCtrl'});
-    $routeProvider.when('/posts/:postId', {templateUrl: 'partials/partial2.html', controller: 'postDetailCtrl'});
+    $routeProvider.when('/', {templateUrl: 'partials/partial_post_list.html', controller: 'postsListCtrl'});
+    $routeProvider.when('/posts/:postId', {templateUrl: 'partials/partial_post_details.html', controller: 'postDetailCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
